b725caf79ffe9588aa464c31c2c57055f0eb8975
Jimmy Rickardsson <Rickardssonjimmy@gmail.com>
2020-09-08 21:14:16 +0200

    added som code

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
+let rowOne = document.getElementsByClassName("rowOneNum");
+
 let dice = document.getElementById("diceBtn");
 
 dice.addEventListener("click", function () {
-  console.log("Du klickade");
+  console.log(rowOne[0].value);
 });
