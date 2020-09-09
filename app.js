09879c81f65d44ac11e45d614143c23c293e5ae1
Jimmy Rickardsson <Rickardssonjimmy@gmail.com>
2020-09-09 11:31:13 +0200

    Nu funkar spelare ett

--- app.js
+++ app.js
@@ -1,20 +1,27 @@
 let rowOne = document.getElementsByClassName("rowOneNum");
 let dice = document.getElementById("diceBtn");
 let calc = document.getElementById("calcBtn");
-
 let totalOne = document.getElementById("totalOne");
-
-// dice.addEventListener("click", function (e) {
-//   console.log(rowOne[0].value);
-// });
+let bonusOne = document.getElementById("bonusOne");
+let sumOne = document.getElementById("sumOne");
 
 calc.addEventListener("click", function () {
   let sum = 0;
+  let sumPlayerOne = 0;
 
   for (let i = 0; i <= 12; i++) {
     sum += rowOne[i].valueAsNumber;
-    totalOne.value = sum;
+    if (sum >= 63) {
+      bonusOne.value = 50;
+    }
   }
 
+  for (let i = 0; i <= 5; i++) {
+    sumPlayerOne += rowOne[i].valueAsNumber;
+    sumOne.value = sumPlayerOne;
+  }
+
+  sum += Number(bonusOne.value);
+  totalOne.value = sum;
   console.log(sum);
 });
