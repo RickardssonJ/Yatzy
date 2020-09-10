51a26d6f886265b8c054124575d3e7be855e9262
Özgür Baserdem <gurr__@hotmail.com>
2020-09-10 09:58:22 +0200

    Added fullhouse function

--- app.js
+++ app.js
@@ -25,3 +25,41 @@ calc.addEventListener("click", function () {
   totalOne.value = sum;
   console.log(sum);
 });
+
+
+// Roll dice function and full house checker
+function rollDice() {
+  let throws = [];
+  let diceThrow = 0;
+  for (i = 0; i < 5; i++) {
+    diceThrow = Math.floor(Math.random() * 6) + 1;
+    throws.push(diceThrow);
+  }
+  console.log(throws);
+  return throws;
+}
+
+let dices = rollDice();
+
+function countDice(dices) {
+  let values = [];
+
+  for (let i = 0; i <= 6; i++) {
+    values[i] = 0;
+  }
+
+  for (let current_dice of dices) {
+    values[current_dice]++;
+  }
+  console.log(values);
+  if (values.includes(2) && values.includes(3)) {
+    alert("You got full house!");
+    console.log("You got full house!");
+    return true;
+  } else {
+    console.log("No full house this round.");
+    return false;
+  }
+}
+
+countDice(dices);
